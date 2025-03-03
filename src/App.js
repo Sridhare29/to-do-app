8b27cc4592fcf9dfa3898474bf8ade03aa362862
Sridhar <sridhar.elumalai@kanini.com>
2025-03-03 23:29:07 +0530

    pretiffer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import CreateTask from './components/CreateTask';
-import Header from './components/Header';
-import TaskList from './components/TaskList';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import CreateTask from "./components/CreateTask";
+import Header from "./components/Header";
+import TaskList from "./components/TaskList";
 
 function App() {
-  const [tasklist, setTasklist] = useState( JSON.parse(localStorage.getItem("tasklist")) || []);
+  const [tasklist, setTasklist] = useState(
+    JSON.parse(localStorage.getItem("tasklist")) || []
+  );
   const [task, setTask] = useState({});
 
-  useEffect(()=> {
-    localStorage.setItem("tasklist", JSON.stringify(tasklist))
-  } , [tasklist]);
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist));
+  }, [tasklist]);
 
   return (
     <div className="App">
-     <Header/>
-     <CreateTask tasklist = {tasklist} setTasklist = {setTasklist} task = {task} setTask = {setTask}/>
-     <TaskList tasklist = {tasklist} setTasklist = {setTasklist} task = {task} setTask = {setTask}/>
+      <Header />
+      <CreateTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
+      <TaskList
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
     </div>
   );
 }
